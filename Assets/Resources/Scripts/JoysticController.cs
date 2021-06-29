5d6861de6e57b1004e7495cfd17a3c6326fa73cb
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-06-30 12:01:27 +1245

    Joystick working as intended

--- Assets/Resources/Scripts/JoysticController.cs
+++ Assets/Resources/Scripts/JoysticController.cs
@@ -7,18 +7,54 @@ public class JoysticController : MonoBehaviour
 {
     [SerializeField] private GameObject JoystickBackground;
     [SerializeField] private GameObject Joystick;
-    private Vector2 joystickVector;
+    public Vector2 JoystickVector { get; set; }
     private Vector2 joystickTouchPos;
-    private Vector2 joystickBG;
+    private Vector2 joystickOrigionalPos;
     private float joystickRadious;
 
 
 
     void Start()
     {
-
-        
+        joystickOrigionalPos = JoystickBackground.transform.position;
+        joystickRadious = JoystickBackground.GetComponent<RectTransform>().sizeDelta.y / 2;
     }
+
+    public void TouchDown()
+    {
+        joystickTouchPos = Input.mousePosition;
+        Joystick.transform.position = Input.mousePosition;
+    }
+
+    public void Drag(BaseEventData baseEventData)
+    {
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        Vector2 dragPos = pointerEventData.position;
+        JoystickVector = (dragPos - joystickTouchPos).normalized;
+
+        float joystickDistance = Vector2.Distance(JoystickVector, joystickOrigionalPos);
+
+        if (joystickDistance < joystickRadious)
+        {
+            Joystick.transform.position = joystickTouchPos + JoystickVector * joystickDistance;
+        }
+
+        else
+        {
+            Joystick.transform.position = joystickOrigionalPos + JoystickVector * joystickRadious;
+        }
+
+
+    }
+
+    public void pointUP()
+    {
+        JoystickVector = Vector2.zero;
+        Joystick.transform.position = joystickOrigionalPos;
+    }
+
+
+
 
     
 }
