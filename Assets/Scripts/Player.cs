3b4f055f49e0f6d7303eddaacebfa11287e56ace
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-07-07 09:51:09 +1245

    Player Movement using new joystick
    player movement with velocity using joystick

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,25 @@ using EasyJoystick ;
 
 public class Player : MonoBehaviour {
 
-   [SerializeField] private float speed ;
-   [SerializeField] private Joystick joystick ;
+    [SerializeField] private float speed;
+    [SerializeField] private Joystick joystick;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-   private void Update () {
-      float xMovement = joystick.Horizontal () ;
-      float zMovement = joystick.Vertical () ;
+    private void Update()
+    {
+        float x = joystick.Horizontal();
+        float y = joystick.Vertical();
 
-      transform.position += new Vector3 (xMovement, 0f, zMovement) * speed * Time.deltaTime ;
-   }
+       // transform.position += new Vector3(x,0, y) * speed * Time.deltaTime;
+        rb.velocity = new Vector2(x, y) * speed;
+
+
+    }
+
+
 
 }
