18bee6b04c9ffa2267db4aacfe723cd8a4c3c486
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-07-07 10:50:51 +1245

    Created second joystick for shooting
    started working on shooting

---
+++ Assets/Resources/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    [SerializeField] private float bulletSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine ;
+using EasyJoystick ;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private float speed;
+    [SerializeField] private float ShootSpeed;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private Joystick moveJoystick;
+    [SerializeField] private Joystick fireJoystick;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform firePos;
+
+    private Rigidbody2D rb;
+    private Rigidbody2D bulletRB;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+
+        bullet = Resources.Load<GameObject>("Prefabs / bullet");
+
+        bulletRB = Resources.Load<Rigidbody2D>("Prefabs / bullet");
+
+
+    }
+
+    private void Update()
+    {
+        float x1 = moveJoystick.Horizontal();
+        float y1 = moveJoystick.Vertical();
+        rb.velocity = new Vector2(x1, y1) * speed;
+
+        if (fireJoystick.Horizontal() != 0 || fireJoystick.Vertical()!= 0)
+        {
+            //Debug.Log("hiiiiiiii");
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        float x = fireJoystick.Horizontal();
+        float y = fireJoystick.Vertical();
+
+        GameObject newBullet = Instantiate(bullet, firePos.position,Quaternion.identity);
+
+        //bulletRB.velocity = new Vector2(x, y) * bulletSpeed;
+
+    }
+
+
+
+}
