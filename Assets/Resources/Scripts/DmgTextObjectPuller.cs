ffb97ea56c37b924b206c72445204a78e75f867f
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-07-13 12:54:45 +1245

    Created new script for damage text and text object Puller

---
+++ Assets/Resources/Scripts/DmgTextObjectPuller.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DmgTextObjectPuller : MonoBehaviour
+{
+    public static DmgTextObjectPuller currentDmgTextObjectPuller;
+    public GameObject PulledDmgTextObject;
+    [SerializeField] private int pullAmount;
+    private bool willGrow = true;
+
+    private List<GameObject> pulledDmgTextObjects;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDmgTextObjectPuller = this;
+        pulledDmgTextObjects = new List<GameObject>();
+
+        for (int i = 0; i < pullAmount; i++)
+        {
+            GameObject obj = Instantiate(PulledDmgTextObject);
+            obj.SetActive(false);
+            pulledDmgTextObjects.Add(obj);
+        }
+    }
+
+    public GameObject DepullDmgTextObject()
+    {
+        for (int i = 0; i < pulledDmgTextObjects.Count; i++)
+        {
+            if (!pulledDmgTextObjects[i].activeInHierarchy)
+            {
+                pulledDmgTextObjects[i].SetActive(true);
+                return pulledDmgTextObjects[i];
+
+            }
+        }
+
+        if (willGrow)
+        {
+            GameObject obj = Instantiate(PulledDmgTextObject);
+            pulledDmgTextObjects.Add(obj);
+            return obj;
+        }
+
+        return null;
+    }
+}
